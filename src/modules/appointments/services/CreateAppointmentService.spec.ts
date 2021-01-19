754d72099c19fad558dc6ea7ce5d6d585929ccd5
Jean Nunes <jaon@itriad.org.br>
2021-01-18 21:47:57 -0400

    Testes de agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,21 +1,29 @@
 import 'reflect-metadata';
 
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', async () => {
+  it('should not be able to create two appointments on the same time', async () => {
     const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-    const createAppointmentService = new CreateAppointmentService(
+    const createAppointment = new CreateAppointmentService(
       fakeAppointmentsRepository,
     );
 
-    const appointment = await createAppointmentService.execute({
-      date: new Date(),
+    const appointmentDate = new Date();
+
+    await createAppointment.execute({
+      date: appointmentDate,
       providerId: '123123',
     });
 
-    expect(appointment).toHaveProperty('id');
-    expect(appointment.providerId).toBe('123123');
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        providerId: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
