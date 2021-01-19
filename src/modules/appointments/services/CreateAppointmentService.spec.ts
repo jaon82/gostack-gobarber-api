3657bfd2239cba2d3cc92f4183aff78fd18e46ae
Jean Nunes <jaon@itriad.org.br>
2021-01-19 18:21:45 -0400

    Recuperação de senha

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -19,7 +19,7 @@ describe('CreateAppointment', () => {
       providerId: '123123',
     });
 
-    expect(
+    await expect(
       createAppointmentService.execute({
         date: appointmentDate,
         providerId: '123123',
---
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -0,0 +1,25 @@
+import { v4 as uuid } from 'uuid';
+
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+
+import UserToken from '../../infra/typeorm/entities/UserToken';
+
+class FakeUserTokensRepository implements IUserTokensRepository {
+  private userTokens: UserToken[] = [];
+
+  public async generate(userId: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: uuid(),
+      token: uuid(),
+      userId,
+    });
+
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+}
+
+export default FakeUserTokensRepository;
